2662c1bdeac0626b29f205be92316fbb066bfca2
your_github_usernamec;s <=>
2022-11-19 19:14:55 +0100

    Initial push

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const NAV_TOGGLER = document.querySelector("#nav-toggler");
+const SIDE_NAV = document.querySelector("#sidenav");
+const MAIN_CONT = document.querySelector("#main");
+NAV_TOGGLER.addEventListener("click", function () {
+    if (SIDE_NAV.classList.contains("navbar-slideout")) {
+        document.querySelectorAll(".abs-menu-item .text").forEach((elem) => elem.style.display = "none");
+        SIDE_NAV.classList.replace("navbar-slideout", "navbar-slidein");
+    }
+    else {
+        document.querySelectorAll(".abs-menu-item .text").forEach((elem) => elem.style.display = "block");
+        SIDE_NAV.classList.replace("navbar-slidein", "navbar-slideout");
+    }
+    if(MAIN_CONT.classList.contains("main-slideout"))
+        MAIN_CONT.classList.replace("main-slideout", "main-slidein");
+    else
+        MAIN_CONT.classList.replace("main-slidein", "main-slideout");
+});
